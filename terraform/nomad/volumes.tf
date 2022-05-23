7770458a406b96235c732d0285a43c805d657279
David Bond <davidsbond93@gmail.com>
2022-05-23 21:16:03 +0100

    Add NFS CSI and set up jobs to use it
    THis commit includes the jobs required for an NFS CSI driver. It
    also modifies my workloads to use CSI persistence instead of relying
    on sticky ephemeral disks.
    
    Signed-off-by: David Bond <davidsbond93@gmail.com>

---
+++ terraform/nomad/volumes.tf
@@ -0,0 +1,71 @@
+resource "nomad_external_volume" "traefik" {
+  type         = "csi"
+  plugin_id    = "nfs"
+  volume_id    = "traefik"
+  name         = "traefik"
+  capacity_min = "10M"
+  capacity_max = "50M"
+
+  capability {
+    access_mode     = "multi-node-multi-writer"
+    attachment_mode = "file-system"
+  }
+
+  lifecycle {
+    prevent_destroy = false
+  }
+}
+
+resource "nomad_external_volume" "home_assistant" {
+  type         = "csi"
+  plugin_id    = "nfs"
+  volume_id    = "home-assistant"
+  name         = "home-assistant"
+  capacity_min = "10M"
+  capacity_max = "1Gi"
+
+  capability {
+    access_mode     = "multi-node-multi-writer"
+    attachment_mode = "file-system"
+  }
+
+  lifecycle {
+    prevent_destroy = false
+  }
+}
+
+resource "nomad_external_volume" "bitwarden" {
+  type         = "csi"
+  plugin_id    = "nfs"
+  volume_id    = "bitwarden"
+  name         = "bitwarden"
+  capacity_min = "10M"
+  capacity_max = "1Gi"
+
+  capability {
+    access_mode     = "multi-node-multi-writer"
+    attachment_mode = "file-system"
+  }
+
+  lifecycle {
+    prevent_destroy = false
+  }
+}
+
+resource "nomad_external_volume" "postgres" {
+  type         = "csi"
+  plugin_id    = "nfs"
+  volume_id    = "postgres"
+  name         = "postgres"
+  capacity_min = "10M"
+  capacity_max = "1Gi"
+
+  capability {
+    access_mode     = "multi-node-multi-writer"
+    attachment_mode = "file-system"
+  }
+
+  lifecycle {
+    prevent_destroy = false
+  }
+}
